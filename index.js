757e840dc89370ac869fa9ab3cbda5a0ab156190
the1812 <the1812@outlook.com>
2019-06-03 22:25:35 +0800

    Add module test

---
+++ index.js
@@ -0,0 +1,3 @@
+import { test } from './test-module/test-module.js'
+
+test()
---
+++ test-module/test-module.js
@@ -0,0 +1,3 @@
+export const test = () => {
+  document.querySelectorAll('.test').forEach(it => (it.innerHTML = 'module test ok'))
+}
